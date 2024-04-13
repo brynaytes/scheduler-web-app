a2eed65eca1d6a988477cf0c2340cb4c749edcc7
BRYNSFRIGATE\bryna <brynaytes@gmail.com>
2024-04-13 12:42:07 -0500

    removing unused component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
-import { KeyManagementComponent } from './key-management/key-management.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MeetingSetupComponent } from './meeting-setup/meeting-setup.component';
 import { MeetingViewComponent } from './meeting-view/meeting-view.component';
-import { TimeEntryComponent } from './time-entry/time-entry.component';
 import { MyMeetingsComponent } from './my-meetings/my-meetings.component';
 
 const routeConfig: Routes = [
@@ -34,16 +32,6 @@ const routeConfig: Routes = [
       path: 'meetingView/:meetingID',
       component: MeetingViewComponent,
       title: 'meetingView'
-    },
-    {
-      path: 'timeEntry',
-      component: KeyManagementComponent,
-      title: 'timeEntry'
-    },
-    {
-      path: 'TimeEntryComponent',
-      component: TimeEntryComponent,
-      title: 'TimeEntryComponent'
     },
     {
       path: 'myMeetings',
