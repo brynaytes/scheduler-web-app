158c6d86f6d4f003e88b29e91e7902e9ccc814b6
BRYNSFRIGATE\bryna <brynaytes@gmail.com>
2024-05-06 23:15:12 -0500

    setup generic dialog menu

--- src/app/meeting-view-public/meeting-view-public.component.ts
+++ src/app/meeting-view-public/meeting-view-public.component.ts
@@ -8,6 +8,8 @@ import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { JwtService } from '../services/jwt/jwt.service';
 import { SpinnerComponent } from '../spinner/spinner.component';
+import { MatDialog } from '@angular/material/dialog';
+import { GenericDialogComponent } from '../generic-dialog/generic-dialog.component';
 
 @Component({
   selector: 'app-meeting-view-public',
@@ -26,7 +28,7 @@ export class MeetingViewPublicComponent {
 
   SelectedDateList: Array<{ date: string; times: Array<{ startTime: string; endTime: string, isAvailable: boolean, availableUsers: Array<string> }> }> = [];
 
-  constructor(public _route: ActivatedRoute, private jwtService: JwtService) {
+  constructor(public _route: ActivatedRoute, private jwtService: JwtService,public dialog: MatDialog) {
     let tempName = this.name = this.jwtService.getClaim(localStorage.getItem("access_token")!, "username");
     if (tempName) {
       this.name = tempName;
@@ -64,5 +66,23 @@ export class MeetingViewPublicComponent {
       dateTimes: this.SelectedDateList
     }
     let response = await RequestHandlerService.sendData(obj, "addTimes", "/meetings", "POST");
+  
+    if(response){
+      this.openDialog("Time Successfully Added","")
+    }else{
+      this.openDialog("An Error has occurred","")
+
+    }
+  }
+
+  openDialog(title : string, text : string): void {
+    this.dialog.open(GenericDialogComponent, {
+      width: '500px',
+      height: '250px',
+      data : {
+        title : title,
+        text : text
+      }
+    });
   }
 } 
