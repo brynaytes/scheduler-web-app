8ce2aee1da297bd8965c86d089585e32095f7c90
BRYNSFRIGATE\bryna <brynaytes@gmail.com>
2024-06-10 00:26:36 -0500

    profile page now updates profile

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,12 +9,14 @@ import { environment } from '../../environments/environment';
     <p>
       login works!
     </p>
-    <a href="{{cognitoUrl}}/oauth2/authorize?client_id=4m9eg4mrjosm9d17clp9iqpdhu&response_type=code&scope=openid+test-resource-server%2Ftest&redirect_uri={{url}}">go </a>
+    <a href="{{cognitoUrl}}/oauth2/authorize?client_id=4m9eg4mrjosm9d17clp9iqpdhu&response_type=code&scope=openid+test-resource-server%2Ftest+aws.cognito.signin.user.admin+email+phone+profile&redirect_uri={{url}}">go </a>
 
   `,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  //all additional scopes must be added directly to the scope section of the href for the login link
+
   url : string = encodeURIComponent(window.location.origin+"/login");
   cognitoUrl = environment.cognitoUrl;
   constructor( public http : HttpClient ) {
