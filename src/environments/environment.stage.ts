a4771629ae946bdfec656a6e87a3124f0b5cbdc7
BRYNS-BATTLESHI\bryna <brynaytes@gmail.com>
2025-05-12 22:36:15 -0500

    removing cognito url /

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
     environment : 'stage',
-    cognitoUrl : "https://scheduler-madness-dev.auth.us-east-1.amazoncognito.com/",
+    cognitoUrl : "https://scheduler-madness-dev.auth.us-east-1.amazoncognito.com",
     isProd : false,
     authUrl : 'https://yy6u5qme97.execute-api.us-east-1.amazonaws.com/dev',
     apiUrl : 'https://yy6u5qme97.execute-api.us-east-1.amazonaws.com/dev'
